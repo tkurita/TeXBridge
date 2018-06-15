d92e8dcb939eda31063e126b17bcec896fbfb665
Tetsuro Kurita <tkurita@mac.com>
2018-06-15 16:48:49 +0900

    aded tag-blue-16@2x.png

--- LabelDatum.m
+++ LabelDatum.m
@@ -6,7 +6,7 @@
 {
 	static NSImage *nodeIcon = nil;
 	//if (!nodeIcon) nodeIcon = [NSImage imageNamed:@"tag-gray-16.png"];
-	if (!nodeIcon) nodeIcon = [NSImage imageNamed:@"tag-blue-16.png"];
+	if (!nodeIcon) nodeIcon = [NSImage imageNamed:@"tag-blue-16"];
 	return nodeIcon;
 }
 
